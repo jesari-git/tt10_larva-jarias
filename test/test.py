702799670fa80fe8e50903d1232cdf71b18fa0e7
jesari-git <158497071+jesari-git@users.noreply.github.com>
2024-11-24 21:06:31 +0100

    Add files via upload

--- test/test.py
+++ test/test.py
@@ -10,14 +10,14 @@ from cocotb.triggers import ClockCycles
 async def test_project(dut):
     dut._log.info("Start")
 
-    # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, units="us")
+    # Set the clock period to 41 us (24 MHz)
+    clock = Clock(dut.clk, 41, units="ns")
     cocotb.start_soon(clock.start())
 
     # Reset
     dut._log.info("Reset")
     dut.ena.value = 1
-    dut.ui_in.value = 0
+    dut.ui_in.value = 8
     dut.uio_in.value = 0
     dut.rst_n.value = 0
     await ClockCycles(dut.clk, 10)
@@ -26,15 +26,226 @@ async def test_project(dut):
     dut._log.info("Test project behavior")
 
     # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+    dut.ui_in.value = 8
+    #dut.uio_in.value = 30
+    await ClockCycles(dut.clk, 208)
+    
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 8
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 8
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 8
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 8
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 8
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 8
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 8
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 8
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 8
+    await ClockCycles(dut.clk, 208)
 
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 8
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 8
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 8
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 8
+    await ClockCycles(dut.clk, 208)
+
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 8
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 8
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 8
+    await ClockCycles(dut.clk, 208)
+
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 8
+    await ClockCycles(dut.clk, 208)
+
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 8
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 8
+    await ClockCycles(dut.clk, 208)
+
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 8
+    await ClockCycles(dut.clk, 208)
+
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 8
+    await ClockCycles(dut.clk, 208)
+
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 8
+    await ClockCycles(dut.clk, 208)
+
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 8
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 8
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 8
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 8
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 8
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 8
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 8
+    await ClockCycles(dut.clk, 208)
+
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 208)
+    dut.ui_in.value = 8
+    await ClockCycles(dut.clk, 208)
+    
     # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
+    await ClockCycles(dut.clk, 10000)
 
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    assert dut.rst_n.value == 1
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
